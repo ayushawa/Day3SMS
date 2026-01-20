a7a57359ee19d1d51c875023dd819f3b096b328d
ayush <devkali849@gmail.com>
2026-01-20 11:49:24 +0530

    Initial commit

---
+++ src/main/java/com/example/day3ms/Day3MsApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3MsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3MsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3ms/Day3MsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3MsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
